4d3f258194f947831f20382fcc58ea2938a60c86
Andrey <burak_andrej@mail.ru>
2021-10-25 18:34:00 +0300

    task-8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://uevyoxihci.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://uevyoxihci.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://task-8-dev2.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
