93a961ff4569ac4eb9f5c090c63fce066d2db11d
Andrey <burak_andrej@mail.ru>
2021-11-07 03:42:26 +0300

    task-9

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'http://aws-task-9-dev4.eu-west-1.elasticbeanstalk.com',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://uevyoxihci.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://uevyoxihci.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://task-8-dev2.eu-west-1.elasticbeanstalk.com/api',
+  bff: 'http://aws-task-9-dev4.eu-west-1.elasticbeanstalk.com',
+  cart: 'http://aws-task-9-dev4.eu-west-1.elasticbeanstalk.com/cart',
 };
 
 export default API_PATHS;
